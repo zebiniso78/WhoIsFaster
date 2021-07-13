5caf3bfdf32cf53974c5f51a101cb09064c83c02
zebiniso78 <73066830+zebiniso78@users.noreply.github.com>
2021-07-13 03:46:48 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ var speedOfPlane=800;
 
 var elForm=document.querySelector(".form");
 var elInputDistance=elForm.querySelector(".input");
-var elResultOfMan=document.querySelector(".result-man");
-var elResultOfBike=document.querySelector(".result-bike");
-var elResultOfCar=document.querySelector(".result-car");
-var elResultOfPlane=document.querySelector(".result-plane");
+var elResultOfMan=elForm.querySelector(".result-man");
+var elResultOfBike=elForm.querySelector(".result-bike");
+var elResultOfCar=elForm.querySelector(".result-car");
+var elResultOfPlane=elForm.querySelector(".result-plane");
 
 elForm.addEventListener("submit", function(evt)
 {
     evt.preventDefault();
 
-    elResultOfMan.textContent=(Number(elInputDistance.value.trim())/speedOfMan);
-    console.log(elResultOfMan.textContent)
-    elResultOfBike.textContent=(Number(elInputDistance.value.trim())/speedOfBicycle);
-    elResultOfCar.textContent=(Number(elInputDistance.value.trim())/speedOfCar);
-    elResultOfPlane.textContent=(Number(elInputDistance.value.trim())/speedOfPlane);
+    elResultOfMan.textContent=(Number(elInputDistance.value.trim())/speedOfMan).toFixed(2)+" Hours";
+    elResultOfBike.textContent=(Number(elInputDistance.value.trim())/speedOfBicycle).toFixed(2)+" Hours";
+    elResultOfCar.textContent=(Number(elInputDistance.value.trim())/speedOfCar).toFixed(2)+" Hours";
+    elResultOfPlane.textContent=(Number(elInputDistance.value.trim())/speedOfPlane).toFixed(2)+" Hours";
 });
 
